f3495f1c811d51f2b7a56408c1ff1ca5e9ef41aa
kiekk <kiekk@i-commtech.com>
2022-10-04 18:03:54 +0900

    add controller TeamController

---
+++ src/main/java/com/example/mds/controller/TeamController.java
@@ -0,0 +1,28 @@
+package com.example.mds.controller;
+
+import com.example.mds.annotation.Controller;
+import com.example.mds.annotation.RequestMapping;
+
+/*
+현재 코드는 에러가 발생.
+UserController, TeamController 모두 같은 요청 url이 있기 때문인데,
+이를 위해 Controller에도 요청 url을 추가해줘야 합니다.
+ */
+@Controller
+public class TeamController {
+
+    @RequestMapping("/join")
+    public void join() {
+        System.out.println("TeamController-join");
+    }
+
+    @RequestMapping("/login")
+    public void login() {
+        System.out.println("TeamController-login");
+    }
+
+    @RequestMapping("/user")
+    public void user() {
+        System.out.println("TeamController-user");
+    }
+}
