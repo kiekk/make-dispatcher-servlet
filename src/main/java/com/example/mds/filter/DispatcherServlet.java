ccf98929929f5ec664545afe5b7ec5dbabe3eb5a
soonho <shyoon991@gmail.com>
2023-01-24 19:24:02 +0900

    DispatcherServlet resolve 로직 수정 - Resolver 사용

--- src/main/java/com/example/mds/filter/DispatcherServlet.java
+++ src/main/java/com/example/mds/filter/DispatcherServlet.java
@@ -1,16 +1,14 @@
 package com.example.mds.filter;
 
-import com.example.mds.annotation.ResponseBody;
 import com.example.mds.entity.MappingRegistry;
 import com.example.mds.entity.RequestHandlerAdapter;
 import com.example.mds.entity.RequestMappingInfo;
+import com.example.mds.resolver.Resolver;
+import com.example.mds.resolver.ResolverFactory;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.PrintWriter;
-import java.lang.reflect.Method;
 
 /*
 매번 요청 정보마다 Servlet을 생성하는 것은 비효율적이기 때문에
@@ -20,9 +18,6 @@ import java.lang.reflect.Method;
 public class DispatcherServlet implements Filter {
 
     private final String basePackage = "com.example.mds.controller";
-    private final String REDIRECT_PREFIX = "redirect:";
-    private final String VIEW_RESOLVER_PREFIX = "/WEB-INF/views/";
-    private final String VIEW_RESOLVER_SUFFIX = ".jsp";
 
     @Override
     public void init(FilterConfig filterConfig) {
@@ -34,7 +29,6 @@ public class DispatcherServlet implements Filter {
         System.out.println("Start Dispatcher");
 
         HttpServletRequest servletRequest = (HttpServletRequest) request;
-        HttpServletResponse servletResponse = (HttpServletResponse) response;
 
         System.out.println("Context Path : " + servletRequest.getContextPath());
         System.out.println("Request URI : " + servletRequest.getRequestURI());
@@ -44,33 +38,8 @@ public class DispatcherServlet implements Filter {
 
         try {
             Object invoke = RequestHandlerAdapter.handle(requestMappingInfo);
-
-            // @ResponseBody 가 있을 경우 MessageConverter로 처리, 없을 경우 ViewResolver로 처리
-            // MessageConverter
-            if (requestMappingInfo.isRestController()) {
-                System.out.println("MessageConverter로 처리된 결과 : " + invoke);
-
-                PrintWriter writer = response.getWriter();
-                writer.println(invoke);
-                writer.close();
-            } else {
-                // ViewResolver
-                System.out.println("ViewResolver로 처리된 결과 : " + invoke);
-
-                String result = String.valueOf(invoke);
-
-                // Redirect 여부 확인
-                if (result.startsWith(REDIRECT_PREFIX)) {
-                    // Redirect
-                    result = result.replaceAll(REDIRECT_PREFIX, "");
-                    servletResponse.sendRedirect(result);
-                } else {
-                    // Forward
-                    RequestDispatcher dispatcher = request.getRequestDispatcher(VIEW_RESOLVER_PREFIX + result + VIEW_RESOLVER_SUFFIX);
-                    dispatcher.forward(servletRequest, servletResponse);
-                }
-            }
-
+            Resolver resolver = ResolverFactory.getResolver(requestMappingInfo);
+            resolver.resolve(request, response, invoke);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
